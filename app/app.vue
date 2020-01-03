2647f9c3027ac3c59e3690b1ff928661de67a015
Silvio Henrique Ferreira <shferreira@me.com>
2020-01-03 10:43:57 +0000

    Add ESLint, Stylelint and Prettier

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <img src="./logo.png">
+    <img src="./logo.png" />
     <div class="hello">
       <h1>Vue.js</h1>
     </div>
@@ -8,17 +8,17 @@
 </template>
 
 <script>
-  export default {
-    name: "App",
-  };
+export default {
+  name: "App"
+};
 </script>
 
 <style lang="stylus" scoped>
-  main
-    font-family "Avenir", Helvetica, Arial, sans-serif
-    -webkit-font-smoothing antialiased
-    -moz-osx-font-smoothing grayscale
-    text-align center
-    color #2c3e50
-    margin-top 60px
+main
+  font-family "Avenir", Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
 </style>
